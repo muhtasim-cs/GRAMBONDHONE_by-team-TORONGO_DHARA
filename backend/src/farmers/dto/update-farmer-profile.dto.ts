b0e4cd95416e0ae87bd79bead30de329fc7fce21
Shamia Akhter Tasfi <samiaakhtertasfi@gmail.com>
2026-09-26 03:42:28 +0600

    Delete backend directory

--- backend/src/farmers/dto/update-farmer-profile.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateFarmerProfileDto } from './create-farmer-profile.dto';
-
-export class UpdateFarmerProfileDto extends PartialType(CreateFarmerProfileDto) {}
